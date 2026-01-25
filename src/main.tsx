9961896e0a9a9a3656a40a161407ff2e698c8e71
moonyking <moonyking.paso@gmail.com>
2026-01-25 21:35:22 +0900

    Update main.tsx
    エントリに Service Worker 登録を追加

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,9 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
+  import { registerSW } from 'virtual:pwa-register'
+
+  registerSW({ immediate: true })
+
   createRoot(document.getElementById("root")!).render(<App />);
   
